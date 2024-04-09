8a4419921ea077d40844c881fc3c330cc6e70eb0
Julyan <0xbitfly@gmail.com>
2024-04-09 17:15:01 +0800

    1

--- .github/script/release.sh
+++ .github/script/release.sh
@@ -13,32 +13,31 @@ echo "Building inscan..."
 RUSTFLAGS="$TARGET_RUSTFLAGS" \
   cargo build --bin inscan --target $TARGET --release
 EXECUTABLE=target/$TARGET/release/inscan
-echo $EXECUTABLE
 
-# if [[ $OS == windows-latest ]]; then
-#   EXECUTABLE=$EXECUTABLE.exe
-# fi
+if [[ $OS == windows-latest ]]; then
+  EXECUTABLE=$EXECUTABLE.exe
+fi
 
-# echo "Copying release files..."
-# mkdir -p dist/inscan-$VERSION
-# cp \
-#   $EXECUTABLE \
-#   Cargo.lock \
-#   Cargo.toml \
-#   readme.md \
-#   $DIST/inscan-$VERSION
+echo "Copying release files..."
+mkdir -p dist/inscan-$VERSION
+cp \
+  $EXECUTABLE \
+  Cargo.lock \
+  Cargo.toml \
+  readme.md \
+  $DIST/inscan-$VERSION
 
-# cd $DIST
-# echo "Creating release archive..."
-# case $OS in
-#   ubuntu-latest | macos-latest)
-#     ARCHIVE=$DIST/inscan-$VERSION-$TARGET.tar.gz
-#     tar czf $ARCHIVE *
-#     echo "::set-output name=archive::$ARCHIVE"
-#     ;;
-#   windows-latest)
-#     ARCHIVE=$DIST/inscan-$VERSION-$TARGET.zip
-#     7z a $ARCHIVE *
-#     echo "::set-output name=archive::`pwd -W`/inscan-$VERSION-$TARGET.zip"
-#     ;;
-# esac
+cd $DIST
+echo "Creating release archive..."
+case $OS in
+  ubuntu-latest | macos-latest)
+    ARCHIVE=$DIST/inscan-$VERSION-$TARGET.tar.gz
+    tar czf $ARCHIVE *
+    echo "::set-output name=archive::$ARCHIVE"
+    ;;
+  windows-latest)
+    ARCHIVE=$DIST/inscan-$VERSION-$TARGET.zip
+    7z a $ARCHIVE *
+    echo "::set-output name=archive::`pwd -W`/inscan-$VERSION-$TARGET.zip"
+    ;;
+esac
